7ab825e382fd5bdf49c16d991aca256543ae394f
Aknathpanchal <100464802+Aknathpanchal@users.noreply.github.com>
2022-08-01 11:01:30 +0530

    parallax done

---
+++ Parallax-Scrolling-React-main/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import ImageOne from './components/ImageOne'
+import ImageTwo from './components/ImageTwo'
+import ImageThree from './components/ImageThree'
+import TextBox from './components/TextBox'
+
+function App() {
+  return (
+    <div className='App'>
+      <ImageOne />
+      <TextBox />
+      <ImageTwo />
+      <TextBox />
+      <ImageThree />
+    </div>
+  );
+}
+
+export default App;
