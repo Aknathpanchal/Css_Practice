c67dadadd3e253b4a1ec2ba2002ef01582656b51
Aknathpanchal <100464802+Aknathpanchal@users.noreply.github.com>
2022-08-13 16:54:34 +0530

    rm game

---
+++ ToolTip/g.js
@@ -0,0 +1,25 @@
+// function fun (a,...b){
+//     console.log(arguments.length);
+
+// }
+// fun(3,"mi","tu")
+
+// const number =[7,6,2,3,8];
+// const max=Math.min.apply(null,number);
+// console.log(max)
+
+// function person(){
+//     console.log(this);
+// }
+
+// person()
+
+// var arr=new Array(5).fill(0).map(function(a,b){
+//     return b+1
+// })
+
+// console.log(arr)
+
+const arr=["a","b","c"]
+arr[0]="d"
+console.log(arr)
