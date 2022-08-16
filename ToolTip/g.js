3b0afaffcea2212ce3d792fd7df2f1dd1f5b3b52
Aknathpanchal <100464802+Aknathpanchal@users.noreply.github.com>
2022-08-16 18:22:18 +0530

    rm da

--- ToolTip/g.js
+++ ToolTip/g.js
@@ -20,6 +20,31 @@
 
 // console.log(arr)
 
-const arr=["a","b","c"]
-arr[0]="d"
-console.log(arr)
+var arr=[10,5,11,9,8]
+// var set= new Set(arr)
+var N=5;
+for(var i=0;i<N;i++){
+    var c=0
+    for(j=0;j<N-i-1;j++){
+        if(arr[j]>arr[j+1]){
+            var temp=arr[j]
+            arr[j]=arr[j+1]
+            arr[j+1]=temp;
+            c++
+        }
+    }
+    if(c==0){
+        break;
+    }
+}console.log(arr)
+//     if(i%3==0 && i%7==0){
+//         console.log("mock")
+//     }else if(i%3==0){
+//         console.log("dsa")
+//     }else if(i%7==0){
+//         console.log("coding")
+//     }else{
+//         console.log(i)
+//     }
+// }
+// console.log(set)
